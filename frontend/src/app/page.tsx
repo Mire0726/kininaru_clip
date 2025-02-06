921abaefedc3078ca35bf4a678e09c6781223515
sumireinoue <violeta.jin.726@gmail.com>
2025-02-06 16:45:43 +0900

    feat: lp画面のui完成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text, Flex } from "@chakra-ui/react";
+import { Box, Button, Image, Text, Flex, HStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -10,28 +10,42 @@ export default function Home() {
       h="100vh"
       textAlign="center"
     >
-      <Box mb={6}>
-        <Text fontSize="6xl" fontWeight="bold" color="white">
-          キニナルクリップ
-        </Text>
-      </Box>
-      <Box mb={4}>
-        <Text fontSize="xl" color="white">
-          行きたい場所・飲食店・ホテル
-          <br />
-          あなたの“気になる”を共有しよう。
-        </Text>
-      </Box>
-      <Box mb={8}>
-        <Text fontSize="md" color="white">
-          キニナルクリップは、行きたい場所や気になっている場所を
-          <br />
-          まとめて他のユーザーに共有できるサービスです。
-        </Text>
-      </Box>
-      <Button colorScheme="blackAlpha" size="lg">
-        はじめる
-      </Button>
+      <HStack spacing={100} mb={4}>
+        <Box>
+          <Box mb={6}>
+            <Text fontSize="6xl" fontWeight="bold" color="white">
+              キニナル
+              <br />
+              クリップ
+            </Text>
+          </Box>
+          <Box mb={4}>
+            <Text fontSize="xl" color="white">
+              行きたい場所・飲食店・ホテル
+              <br />
+              あなたの“気になる”を共有しよう。
+            </Text>
+          </Box>
+          <Box mb={8}>
+            <Text fontSize="md" color="white">
+              キニナルクリップは、行きたい場所や気になっている場所を
+              <br />
+              まとめて他のユーザーに共有できるサービスです。
+            </Text>
+          </Box>
+          <Button colorScheme="blackAlpha" size="lg">
+            はじめる
+          </Button>
+        </Box>
+        <Box>
+          <Image
+            src="/icon.svg"
+            alt="キニナルクリップ アイコン"
+            width={335}
+            height={335}
+          />
+        </Box>
+      </HStack>
     </Flex>
   );
 }
