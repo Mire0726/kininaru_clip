f512eb18d19277c895271b483ce86e599df801bf
Yu Usui <131258493+Uyyuu@users.noreply.github.com>
2025-02-11 17:26:45 +0900

    Feat/イベントIDから、アイデアを全て取得するAPIの実装 (#14)
    * feat/Get IdeasのAPIの実装
    
    * fix/スペルミス
    
    * feat: goでのenum
    
    * fix/IdeaTagを使用して、gormの条件式を書き換え。
    
    ---------
    
    Co-authored-by: sumireinoue <violeta.jin.726@gmail.com>

--- backend/domain/model/idea.go
+++ backend/domain/model/idea.go
@@ -5,17 +5,33 @@ type Idea struct {
 	Title     string
 	Url       string
 	CreatedBy string
-	Tag       string
+	Tag       IdeaTag
 	EventID   string
 	Likes     int32
 	Summary   *string
 	Memo      *string
 }
 
+type  IdeaTag string
+
+const (
+	Location   IdeaTag = "location"
+	Restaurant IdeaTag = "restaurant"
+	Hotel      IdeaTag = "hotel"
+	Other      IdeaTag = "other"
+)
+
 type CreateIdeaInput struct {
 	Title     string
 	Url       string
 	CreatedBy string
-	Tag       string
+	Tag       IdeaTag
 	EventID   string
+}
+
+type GetIdeasReponse struct {
+	Location   []*Idea
+	Restaurant []*Idea
+	Hotel      []*Idea
+	Other      []*Idea
 }
