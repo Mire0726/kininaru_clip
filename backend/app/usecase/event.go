a8fe42d0abc90a4611b45fafc434395b6b4ebe9d
sumireinoue <violeta.jin.726@gmail.com>
2025-02-12 19:25:26 +0900

    fix: すみませんcreate event修正します

--- backend/app/usecase/event.go
+++ backend/app/usecase/event.go
@@ -30,13 +30,28 @@ func NewEventUsecase(data datastore.Data, log *log.Logger) EventUsecase {
 func (e *eventUC) Create(ctx context.Context, input *model.CreateEventInput) (*model.Event, error) {
 	id := uid.NewGenerator().NewULID()
 	event := &model.Event{
-		ID:        id,
-		Title:     input.Title,
-		URL:       "group/" + id,
+		ID:    id,
+		Title: input.Title,
+		URL:   "group/" + id,
 	}
 
 	if err := e.data.ReadWriteStore().Event().Create(ctx, event); err != nil {
 		e.log.Error("failed to create event")
+
+		return nil, err
+	}
+
+	users := make([]*model.User, 0, len(input.Users))
+	for _, user := range input.Users {
+		users = append(users, &model.User{
+			ID:      uid.NewGenerator().NewULID(),
+			Name:    user.Name,
+			EventID: id,
+		})
+	}
+
+	if err := e.data.ReadWriteStore().User().BulkCreate(ctx, users); err != nil {
+		e.log.Error("failed to create users")
 
 		return nil, err
 	}
