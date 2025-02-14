d8f6d5688ff5981183ed768d47a1cf632bb5cbf4
Yu Usui <131258493+Uyyuu@users.noreply.github.com>
2025-02-14 19:17:48 +0900

    build/python環境をdockerで構築 (#25)

---
+++ ai_engine/src/maps/places.py
@@ -0,0 +1,64 @@
+import os
+from typing import Tuple, List, Dict
+from urllib.parse import urlparse, unquote
+from dotenv import load_dotenv
+import googlemaps
+
+load_dotenv()
+
+GOOGLEMAP_API_KEY = os.environ.get("GOOGLEMAP_API_KEY")
+
+
+def googlemap_url_parser(url: str) -> Tuple[str, float, float]:
+    """
+    url : GoogleMap URL
+
+    Tuple[str, float, float] : location_name, latitude, longtiude
+    """
+    # short urlからlong urlへの変換。ただスクレイピングみたいな判定されて弾かれる。
+    # response = requests.get(url, allow_redirects=False)
+    # long_url = response.url
+
+    parsed_url = urlparse(url)
+    encoded_place_names: List[str] = parsed_url.path.split("/")[3].split("+")
+    location_info = parsed_url.path.split("/")[4].split(",")
+    latitude: float = float(location_info[0][1:])
+    longtitude: float = float(location_info[1])
+
+    location_name: str = ""
+    for encoded_place_name in encoded_place_names:
+        if location_name == "":
+            location_name += unquote(encoded_place_name)
+            continue
+
+        location_name += f" {unquote(encoded_place_name)}"
+
+    return location_name, latitude, longtitude
+
+
+def get_place_info_for_summary(url: str) -> Dict[str, str]:
+    name, latitude, longtitude = googlemap_url_parser(url)
+    gmaps_client: googlemaps.Client = googlemaps.Client(key=GOOGLEMAP_API_KEY)
+    place_result: Dict[str] = gmaps_client.places(
+        query=name, location=(latitude, longtitude)
+    )
+    place_id: str = place_result["results"][0]["place_id"]
+    print(place_id)
+
+    place_details_result = gmaps_client.place(place_id=place_id, language="ja")
+    summary_info = {}
+    summary_info["name"] = place_details_result["result"].get("name")
+    summary_info["types"] = place_details_result["result"].get("types")
+    summary_info["rating"] = place_details_result["result"].get("rating")
+    summary_info["reviews"] = place_details_result["result"].get("reviews", [])[:5]
+
+    print(summary_info)
+
+    return summary_info
+
+
+if __name__ == "__main__":
+    url = "https://www.google.co.jp/maps/place/%E6%9C%AD%E5%B9%8C%E5%B8%82%E6%99%82%E8%A8%88%E5%8F%B0/@43.0628859,141.3509121,17z/data=!4m6!3m5!1s0x5f0b297627507247:0x1b9ba84a4b04cdeb!8m2!3d43.0625768!4d141.3534928!16s%2Fm%2F0263ys5?entry=ttu&g_ep=EgoyMDI1MDIxMC4wIKXMDSoASAFQAw%3D%3D"
+    place_info = get_place_info_for_summary(url)
+
+    print(place_info)
---
+++ ai_engine/src/services/summary.py
@@ -0,0 +1,56 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+
+from dotenv import load_dotenv
+from src.maps.places import get_place_info_for_summary
+from src.llm.llm import LLM
+
+load_dotenv()
+
+
+def summarize_map_info(url: str, debug: bool = False) -> str:
+    place_info = get_place_info_for_summary(url)
+
+    prompt = """
+            ### 指示
+            INPUTの内容を見て、SITUATIONに合わせた、OUTPUT FORMATのようなまとめを作ってください。
+            
+            ### SITUATION
+            - 友達との旅行の行きたいリストの共有で、どんな店か簡単に理解するためのまとめ。
+            - プライベートでの友達や家族との使用です。その状況を考慮して、カジュアルな感じでお願いします。
+            - 一つ一つは端的な短めな文章でお願いします。
+
+            ### INPUT
+            <<INPUT>>
+
+            ### OUTPUT FORMAT
+            - ジャンル：
+            - 雰囲気：
+            - おすすめ：
+            - 評価：
+
+            この文章をsummaryをキーとするjsonで生成してください。
+            OUTPUT FORMAT自体は、全体で文章にしてください。
+            また、評価は星を使って視覚的にもわかりやすいようにしてください。
+            """
+
+    client = LLM(base="openai", model="gpt-4o-mini")
+    prompt = prompt.replace("<<INPUT>>", str(place_info))
+    client.set_prompt(prompt)
+
+    if debug:
+        print(client.prompt)
+
+    res, _, _ = client.get_response()
+
+    return res
+
+
+if __name__ == "__main__":
+    url = "https://www.google.co.jp/maps/place/%E6%9C%AD%E5%B9%8C%E5%B8%82%E6%99%82%E8%A8%88%E5%8F%B0/@43.0628859,141.3509121,17z/data=!4m6!3m5!1s0x5f0b297627507247:0x1b9ba84a4b04cdeb!8m2!3d43.0625768!4d141.3534928!16s%2Fm%2F0263ys5?entry=ttu&g_ep=EgoyMDI1MDIxMC4wIKXMDSoASAFQAw%3D%3D"
+    res = summarize_map_info(url)
+
+    print(res)
