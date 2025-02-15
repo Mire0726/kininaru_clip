a0754032f9504e8408bb261c3a7c2cd8649f7449
sumireinoue <violeta.jin.726@gmail.com>
2025-02-16 04:43:29 +0900

    mod

--- frontend/src/app/group/[id]/ideas/[ideaId]/recommendSection.tsx
+++ frontend/src/app/group/[id]/ideas/[ideaId]/recommendSection.tsx
@@ -1,6 +1,16 @@
-import { Box, Text, Link, VStack, Flex, Icon, Spinner } from "@chakra-ui/react";
-import { FaUtensils, FaHotel, FaMapMarkerAlt, FaStar } from "react-icons/fa";
-import { FetchRecommendResponse} from "@/constants/type";
+import {
+  Box,
+  Text,
+  Link,
+  VStack,
+  Flex,
+  Icon,
+  Spinner,
+  Divider,
+} from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { FetchRecommendResponse } from "@/constants/type";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 interface RecommendSectionProps {
   recommends?: FetchRecommendResponse;
@@ -19,21 +29,32 @@ const RecommendSection = ({ recommends, isLoading }: RecommendSectionProps) => {
   return (
     <VStack spacing={4} align="stretch" mt={4}>
       {recommends.recommends.map((recommend) => (
-        <Box key={recommend.id} p={4} bg="white" borderRadius="md" boxShadow="md">
+        <Box
+          key={recommend.id}
+          p={4}
+          bg="white"
+          borderRadius="md"
+          boxShadow="md"
+        >
+          <Text fontSize="md" color="gray.500" mt={2}>
+            ここもおすすめ！⭐️
+          </Text>
           <Flex justify="space-between">
-            <Text fontSize="lg" fontWeight="bold">
-              おすすめ⭐️ {recommend.name}
-            </Text>
+            <Heading size="md" mt={2}>
+              {recommend.name}
+            </Heading>
             <Link href={recommend.url} isExternal>
-              <Icon as={FaStar} color="yellow.500" />
+              <Icon as={ExternalLinkIcon} color="gray.600" />
             </Link>
           </Flex>
-          <Text fontSize="sm" color="gray.500" mt={2}>
-            {recommend.url}
+          <Divider my={4} />
+
+          <Text fontSize="md" fontWeight="bold">
+            おすすめポイント：
           </Text>
           <Flex mt={2}>
             <Text whiteSpace="pre-wrap" wordBreak="break-word">
-              おすすめポイント：{recommend.content}
+              {recommend.content}
             </Text>
           </Flex>
         </Box>
