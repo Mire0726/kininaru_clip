2043168d973d70d67a81216413548d8b07002b40
sumireinoue <violeta.jin.726@gmail.com>
2025-02-16 13:45:18 +0900

    fix:

--- frontend/src/app/group/[id]/ideas/[ideaId]/page.tsx
+++ frontend/src/app/group/[id]/ideas/[ideaId]/page.tsx
@@ -118,6 +118,7 @@ export default function IdeaList({ params }: Props) {
         },
         requestBody: {
           title: editedTitle,
+          summary: idea?.summary,
           memo: editedMemo,
         },
       },
