55e044e0ac72b134311a2a2ac12dbc23982a303d
sumireinoue <violeta.jin.726@gmail.com>
2025-02-28 20:44:10 +0900

    configの設定

--- frontend/src/constants/config.tsx
+++ frontend/src/constants/config.tsx
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-    BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL 
-  } as const;
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+} as const;
+
+export type ApiConfig = typeof API_CONFIG;
