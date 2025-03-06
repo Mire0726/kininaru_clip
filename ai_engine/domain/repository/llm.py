c1e5c8e83c70856e513ce51b1dbcd00b06aabc0b
Yu Usui <131258493+Uyyuu@users.noreply.github.com>
2025-03-07 01:17:31 +0900

    feat: ml/llmの実装

--- ai_engine/domain/repository/llm.py
+++ ai_engine/domain/repository/llm.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 
+from pydantic import BaseModel
+
 
 class LLMRepository(ABC):
     @abstractmethod
-    def get_resoponse(prompt: str) -> str:
+    def get_resoponse(self, prompt: str, respons_format: BaseModel) -> str:
         pass
---
+++ ai_engine/ml/llm/openai.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+from pydantic import BaseModel
+
+from domain.repository.llm import LLMRepository
+
+load_dotenv()
+
+
+class OpenAILLM(LLMRepository):
+    def __init__(self):
+        self.client: OpenAI = OpenAI()
+
+    def _build_prompt(self, prompt: str) -> list[dict]:
+        return [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                ],
+            }
+        ]
+
+    def get_resoponse(self, prompt: str, respons_format: BaseModel) -> str:
+        message: list[dict] = self._build_prompt(prompt=prompt)
+        response = self.client.beta.chat.completions.parse(
+            model="gpt-4o-mini",
+            messages=message,
+            temperature=1,
+            max_tokens=4096,
+            top_p=1.0,
+            frequency_penalty=0,
+            presence_penalty=0,
+            response_format=respons_format,
+        )
+        content: str = response.choices[0].message.content
+
+        return content
