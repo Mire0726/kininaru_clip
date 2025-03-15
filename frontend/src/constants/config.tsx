01f351dfc9a17c6d027b33028bfde6a495ef13ee
sumireinoue <violeta.jin.726@gmail.com>
2025-03-15 19:47:19 +0900

    configの設定

--- frontend/src/constants/config.tsx
+++ frontend/src/constants/config.tsx
@@ -2,4 +2,8 @@ export const API_CONFIG = {
   BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
 } as const;
 
+if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
+  console.warn("Warning: NEXT_PUBLIC_API_BASE_URL is not defined");
+}
+
 export type ApiConfig = typeof API_CONFIG;
