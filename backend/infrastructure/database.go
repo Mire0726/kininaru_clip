211e57f5f08cc9373f0fb64c2bd9570c3aafe552
Yu Usui <131258493+Uyyuu@users.noreply.github.com>
2025-03-16 00:28:52 +0900

    fix: db接続失敗時に、リトライ処理を実装

--- backend/infrastructure/database.go
+++ backend/infrastructure/database.go
@@ -2,19 +2,43 @@ package infrastructure
 
 import (
 	"fmt"
+	"time"
+
+	"kininaru_clip/backend/pkg/log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
+func connectDB(dialector gorm.Dialector, maxRetries int) (*gorm.DB, error) {
+	for i := 0; i < maxRetries; i++ {
+		db, err := gorm.Open(dialector, &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+
+		if err != nil {
+			log.Error("failed to connect database")
+		} else {
+			log.Info("Success connect to database!!")
+			return db, nil
+		}
+
+		log.Info("Retrying in 10 sec")
+		time.Sleep(10 * time.Second)
+	}
+
+	return nil, fmt.Errorf("could not connect to tha database after %d retries", maxRetries)
+}
+
 func NewDB() (*gorm.DB, error) {
 	// PostgreSQL接続文字列
 	dsn := "host=postgresql user=postgres password=postgres dbname=db port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	dialector := postgres.Open(dsn)
+	maxRetries := 5
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+	db, err := connectDB(dialector, maxRetries)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect database: %w", err)
 	}
