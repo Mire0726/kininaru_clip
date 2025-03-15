fd753d833a5d6c5dd9700c0b86886e27fffcff78
sumireinoue <violeta.jin.726@gmail.com>
2025-03-16 01:23:49 +0900

    デフォルト値削除

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL ,
   },
   // 環境変数のバリデーション
   onDemandEntries: {
